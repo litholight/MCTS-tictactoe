3bbd69ead837e4b90c9fa8689f50199206a16fcc
Sean Stoneburner <litholight@gmail.com>
2018-12-22 15:49:27 -0500

    created arrays for x moves and y moves.

--- game.js
+++ game.js
@@ -1,26 +1,39 @@
 let turn = 1;
 
-function playerTurn() {
+let xMoves = [];
+let yMoves = [];
+let winMoves = [[A1,A2,A3],[],[]];
+
+function playerMove(cell) {
+
   turn++;
   if (turn % 2 === 0) {
+    xMoves.push(cell);
+    console.log("xMoves", xMoves);
     return "X";
   } else {
-    return "0"
+    yMoves.push(cell);
+    console.log("yMoves", yMoves);
+    return "O"
   }
 }
 
+// function hasWon() {
+//
+// }
+
 
 // cells in row A
-document.getElementById("tdA1").addEventListener("click", function(){this.innerHTML = playerTurn();});
-document.getElementById("tdA2").addEventListener("click", function(){this.innerHTML = playerTurn();});
-document.getElementById("tdA3").addEventListener("click", function(){this.innerHTML = playerTurn();});
+document.getElementById("A1").addEventListener("click", function(){this.innerHTML = playerMove("A1");});
+document.getElementById("A2").addEventListener("click", function(){this.innerHTML = playerMove("A2");});
+document.getElementById("A3").addEventListener("click", function(){this.innerHTML = playerMove("A3");});
 
 // cells in row B
-document.getElementById("tdB1").addEventListener("click", function(){this.innerHTML = playerTurn();});
-document.getElementById("tdB2").addEventListener("click", function(){this.innerHTML = playerTurn();});
-document.getElementById("tdB3").addEventListener("click", function(){this.innerHTML = playerTurn();});
+document.getElementById("B1").addEventListener("click", function(){this.innerHTML = playerMove("B1");});
+document.getElementById("B2").addEventListener("click", function(){this.innerHTML = playerMove("B2");});
+document.getElementById("B3").addEventListener("click", function(){this.innerHTML = playerMove("B3");});
 
 // cells in row C
-document.getElementById("tdC1").addEventListener("click", function(){this.innerHTML = playerTurn();});
-document.getElementById("tdC2").addEventListener("click", function(){this.innerHTML = playerTurn();});
-document.getElementById("tdC3").addEventListener("click", function(){this.innerHTML = playerTurn();});
+document.getElementById("C1").addEventListener("click", function(){this.innerHTML = playerMove("C1");});
+document.getElementById("C2").addEventListener("click", function(){this.innerHTML = playerMove("C2");});
+document.getElementById("C3").addEventListener("click", function(){this.innerHTML = playerMove("C3");});
